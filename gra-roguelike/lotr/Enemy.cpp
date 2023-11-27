8dde447af9af1f3925b9713539685b1ab2cd5fe1
plastik3 <126866176+plastik3@users.noreply.github.com>
2023-11-27 20:33:43 +0100

    Delete gra-roguelike directory

--- gra-roguelike/lotr/Enemy.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "Enemy.h"
-
-Enemy::Enemy() {}
--- gra-roguelike/lotr/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include "Game.h"
-
-
-int main() {
-    Game game("level.txt");
-    game.playGame();
-}
