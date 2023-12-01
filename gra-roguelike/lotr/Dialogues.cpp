cdc5f03c7702befbcc4b4e0da5f28b108a8e2f96
plastik3 <126866176+plastik3@users.noreply.github.com>
2023-12-01 14:56:42 +0100

    Delete gra-roguelike directory

--- gra-roguelike/lotr/Dialogues.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by melka on 28.11.2023.
-//
-
-#include "Dialogues.h"
--- gra-roguelike/lotr/Enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "Enemy.h"
-
-Enemy::Enemy() {}
-
-
-void Enemy::setWeakness() {
-    weakness = rand() % sizeof(Trait);
-
-}
